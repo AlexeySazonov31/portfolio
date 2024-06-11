712d96f7cbe82df1cb97bea9744efccc36c88696
Aliaksei Sazonau <sazonleha010203@gmail.com>
2024-06-11 22:23:05 +0300

    tw config

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,3 @@
+export function Home() {
+  return <h1 className="text-2xl text-red-600">Homee</h1>;
+}
---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite';
+import preact from '@preact/preset-vite';
+import liveReload from 'vite-plugin-live-reload'
+
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	plugins: [
+		preact({
+			prerender: {
+				enabled: true,
+				renderTarget: '#app',
+				additionalPrerenderRoutes: ['/404'],
+			},
+		}),
+		liveReload('./src/**/**/*.{ts,tsx}'),
+	],
+});
